5f7ac3f859ed2752c775cdc900e02b6f1223f1a4
Richardy_Rodrigues <richardyrodrigues519@gmail.com>
2025-04-27 14:07:54 +0000

    Criando tela cadastro

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,132 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    print('Email: $email');
+    print('Senha: $password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/pokeball.gif',
+              height: 40,
+              width: 40,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              'Pokedex',
+              style: GoogleFonts.limelight(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Colors.red,
+        elevation: 0,
+      ),
+      body: Container(
+        color: Color(0xffffffff),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  'assets/squirtle1.png',
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.dmSerifDisplay(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff000000)),
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Color(0xff000000)),
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Color(0xff000000)),
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Não tem um cadastro?'),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Cadastre-se',
+                        style: TextStyle(color: Colors.blue),
+                      ))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
